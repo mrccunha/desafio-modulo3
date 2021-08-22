52b0c1a85f14b5d97de730a00082edac29085500
Mauro Roberto da Cruz Cunha <mrccunha@gmail.com>
2021-08-21 21:20:20 -0300

    Desafio Módulo 3

---
+++ src/server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const dataBase = require('./dataBase')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+// Mostrar todos os Pokemons
+app.get('/pokemons', (req, res) => {
+    res.send(dataBase.mostrarPokemons())
+})
+
+//Mostrar apenas um Pokemon
+//Para passar parâmento usa o :
+app.get('/pokemons/:id', (req, res) => {
+    res.send(dataBase.mostrarPokemon(req.params.id))
+})
+
+app.post('/pokemons', (req, res) => {
+    const pokemon = dataBase.salvarPokemons({
+        nome: req.body.nome,
+        tipo: req.body.tipo,
+        fraqueza: req.body.fraqueza,
+        resistencia: req.body.resistencia,
+        hp: 100
+    })
+    res.send(pokemon)    
+})
+
+app.put('/pokemons/:id', (req, res) => {
+    const pokemon = dataBase.atualizarPokemon(req.params.id, {
+        nome: req.body.nome,
+        tipo: req.body.tipo,
+        fraqueza: req.body.fraqueza,
+        resistencia: req.body.resistencia,
+        hp: 100,
+        id: parseInt(req.params.id)    
+    })
+    res.send(pokemon)
+})
+
+app.delete('/pokemons/:id', (req, res) => {
+    res.send(dataBase.deletarPokemons(req.params.id))
+})
+
+app.post('/batalha', (req, res) => {
+    res.send(dataBase.batalhaPokemon(req.body.id1, req.body.id2))
+})
+
+app.post('/curar/:id', (req, res) => {
+    res.send(dataBase.curarPokemon(req.params.id))
+})
+
+app.listen(3003)
